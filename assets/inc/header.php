a9054113110bd74e77cd7a4fcaf9f28334aef0e8
andre <79812@rocteraa-student.nl>
2020-06-29 16:01:29 +0200

    First commit
    Made folder structure, added the navbar, background, and bootstrap.

---
+++ assets/inc/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>BackYardBBQ</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <!-- <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico"> -->
+    <!-- <link rel="icon" sizes="192x192" href="assets/img/logo.png"> -->
+</head>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">BackYardBBQ</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="./index.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./contact.php">Contact</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./about.php">Over ons</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./assortiment.php">Assortiment</a>
+      </li>
+    </ul>
+  </div>
+</nav>
