51e545df50880a9a5b137c8c815d329110034940
andre <79812@rocteraa-student.nl>
2020-06-30 15:44:05 +0200

    30-6-2020
    Clean homepage with video background and animation, tried to add login page, register page not working atm will try again tomorrow

--- assets/inc/header.php
+++ assets/inc/header.php
@@ -4,30 +4,30 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>BackYardBBQ</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="icon" type="image/x-icon" href="assets/media/favicon.ico">
     <link rel="stylesheet" href="./assets/css/style.css">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <!-- <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico"> -->
-    <!-- <link rel="icon" sizes="192x192" href="assets/img/logo.png"> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">BackYardBBQ</a>
+<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+  <a class="navbar-brand ml-5" href="index.php"><img src="./assets/media/logo.png"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="./index.php">Home</a>
+    <ul class="navbar-nav ml-auto mr-5">
+      <li class="nav-item">
+        <a class="nav-link" href="./index.php"><i class="fa fa-fw fa-home"></i> Home</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="./contact.php">Contact</a>
+        <a class="nav-link" href="./contact.php"><i class="fa fa-fw fa-envelope"></i> Contact</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="./about.php">Over ons</a>
+        <a class="nav-link" href="./about.php"><i class="fa fa-fw fa-phone"></i> Over ons</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="./assortiment.php">Assortiment</a>
+        <a class="nav-link" href="./assortiment.php"><i class="fa fa-fw fa-shopping-cart"></i> Assortiment</a>
       </li>
     </ul>
   </div>
--- index.php
+++ index.php
@@ -1,5 +1,18 @@
+<video autoplay muted loop id="backgroundVideo">
+  <source src="./assets/media/bbq.mp4" type="video/mp4">
+</video>
 <?php include('./assets/inc/header.php') ?>
 <body>
+<div id="main-text">
+	BackYardBBQ
+</div>
+<div id="info-text" class="mr-5">
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus pulvinar massa ex. <br>
+	Sed dignissim neque sodales sagittis. <br>
+	Nam sed elit ipsum. Curabitur fringilla mollis nunc ac imperdiet. Quisque viverra mi in condimentum semper.<br>
+	Ut gravida nunc id dui aliquam, non placerat sapien rhoncus. Pellentesque feugiat ac ex tristique consequat.<br>
+	<a type="button" class="btn btn-dark mt-2 btn-lg" href="./assortiment.php">Naar assortiment</a>
+</div>
 
 </body>
 </html>
