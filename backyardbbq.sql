8c0d238fe805bb9b4e1476fa117f1f7f72b681d4
andre <79812@rocteraa-student.nl>
2020-07-03 12:26:46 +0200

    Database backup
    updated database

--- backyardbbq.sql
+++ backyardbbq.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jul 01, 2020 at 01:42 PM
+-- Generation Time: Jul 03, 2020 at 10:25 AM
 -- Server version: 5.7.24
 -- PHP Version: 7.2.11
 
@@ -39,7 +39,54 @@ CREATE TABLE `account` (
 --
 
 INSERT INTO `account` (`email`, `username`, `password`) VALUES
-('admin@admin.com', 'admin', '$2y$10$6WyOOCqjQ2eZ4oklrxuN5u3KMhkR/CpklZ5cVdJd3UGFo6MY8rHSi');
+('admin@admin.com', 'admin', '$2y$10$6WyOOCqjQ2eZ4oklrxuN5u3KMhkR/CpklZ5cVdJd3UGFo6MY8rHSi'),
+('admin1@admin.com', 'admin1', '$2y$10$iYnIbeOTgkgDNkK72Kz42eAUhb9ZfL1ls9DH3ORIWOoGCsX.DMsCe');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `orderId` int(11) NOT NULL,
+  `clientFirstName` varchar(255) NOT NULL,
+  `clientLastName` varchar(255) NOT NULL,
+  `clientStreetName` varchar(255) NOT NULL,
+  `clientHouseNumber` varchar(255) NOT NULL,
+  `clientCity` varchar(255) NOT NULL,
+  `clientZipcode` varchar(10) NOT NULL,
+  `clientPhoneNumber` varchar(20) NOT NULL,
+  `clientEmail` varchar(255) NOT NULL,
+  `orderStartDate` date NOT NULL,
+  `orderDuration` int(3) NOT NULL,
+  `orderDelivery` tinyint(1) NOT NULL,
+  `productId` int(5) NOT NULL,
+  `orderDate` datetime NOT NULL,
+  `orderPrice` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `productcategories`
+--
+
+CREATE TABLE `productcategories` (
+  `productCategoryId` int(3) NOT NULL,
+  `productCategoryTitle` varchar(255) NOT NULL,
+  `productCategoryImage` varchar(255) NOT NULL,
+  `productCategoryDescription` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `productcategories`
+--
+
+INSERT INTO `productcategories` (`productCategoryId`, `productCategoryTitle`, `productCategoryImage`, `productCategoryDescription`) VALUES
+(1, 'Big Green Egg', 'biggreenegg.png', ''),
+(2, 'Kamado Joe', '', ''),
+(3, 'The Bastard', '', '');
 
 -- --------------------------------------------------------
 
@@ -48,14 +95,25 @@ INSERT INTO `account` (`email`, `username`, `password`) VALUES
 --
 
 CREATE TABLE `products` (
-  `id` int(11) NOT NULL,
-  `productName` int(11) NOT NULL,
-  `productInfo` text NOT NULL,
-  `productWeight` int(11) NOT NULL,
+  `productId` int(5) NOT NULL,
+  `productTitle` varchar(255) NOT NULL,
+  `productDescription` text NOT NULL,
+  `productWeight` decimal(5,2) NOT NULL,
   `productDimensions` varchar(20) NOT NULL,
-  `productType` varchar(25) NOT NULL,
-  `productPrice` int(11) NOT NULL
+  `productPricePerDay` decimal(5,2) NOT NULL,
+  `productCategoryId` int(5) NOT NULL,
+  `productImage` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`productId`, `productTitle`, `productDescription`, `productWeight`, `productDimensions`, `productPricePerDay`, `productCategoryId`, `productImage`) VALUES
+(1, 'Big Green Egg Medium', 'aksjdaskdkasdhasjkdhasjkdhasdkjashdjkasdhasdhaskjdhsjkaksjdaskdkasdhasjkdhasjkdhasdkjashdjkasdhasdhaskjdhsjkaksjdaskdkasdhasjkdhasjkdhasdkjashdjkasdhasdhaskjdhsjkaksjdaskdkasdhasjkdhasjkdhasdkjashdjkasdhasdhaskjdhsjk', '50.00', '120x50x50', '30.00', 1, 'biggreenegg.png'),
+(2, 'Big Green Egg Large', 'aksjdaskdkasdhasjkdhasjkdhasdkjashdjkasdhasdhaskjdhsjkaksjdaskdkasdhasjkdhasjkdhasdkjashdjkasdhasdhaskjdhsjkaksjdaskdkasdhasjkdhasjkdhasdkjashdjkasdhasdhaskjdhsjkaksjdaskdkasdhasjkdhasjkdhasdkjashdjkasdhasdhaskjdhsjk', '60.00', '150x60x60', '40.00', 1, 'biggreenegg.png'),
+(3, 'Big Green Egg XtraLarge', 'aksjdaskdkasdhasjkdhasjkdhasdkjashdjkasdhasdhaskjdhsjkaksjdaskdkasdhasjkdhasjkdhasdkjashdjkasdhasdhaskjdhsjkaksjdaskdkasdhasjkdhasjkdhasdkjashdjkasdhasdhaskjdhsjkaksjdaskdkasdhasjkdhasjkdhasdkjashdjkasdhasdhaskjdhsjk', '70.00', '180x70x60', '70.00', 1, 'biggreenegg.png'),
+(4, 'Kamado Joe Medium', 'aksjdaskdkasdhasjkdhasjkdhasdkjashdjkasdhasdhaskjdhsjkaksjdaskdkasdhasjkdhasjkdhasdkjashdjkasdhasdhaskjdhsjkaksjdaskdkasdhasjkdhasjkdhasdkjashdjkasdhasdhaskjdhsjkaksjdaskdkasdhasjkdhasjkdhasdkjashdjkasdhasdhaskjdhsjk', '40.00', '120x70x60', '40.00', 2, 'kamadojoe.png');
 
 -- --------------------------------------------------------
 
@@ -74,20 +132,55 @@ CREATE TABLE `users` (
 --
 
 --
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`orderId`);
+
+--
+-- Indexes for table `productcategories`
+--
+ALTER TABLE `productcategories`
+  ADD PRIMARY KEY (`productCategoryId`);
+
+--
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`productId`),
+  ADD KEY `productCategoryId` (`productCategoryId`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `orderId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `productcategories`
+--
+ALTER TABLE `productcategories`
+  MODIFY `productCategoryId` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `productId` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`productCategoryId`) REFERENCES `productcategories` (`productCategoryId`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
